6f85bd94b04c03d41fc30d3a4703c72b5463323b
Sergio Andres Virviescas Santana <developersavsgio@gmail.com>
2019-01-19 19:29:23 +0100

    Upgrade dependencies

--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module github.com/savsgio/dictpool
 
 require (
-	github.com/philhofer/fwd v1.0.0
-	github.com/savsgio/gotils v0.0.0-20181019073832-3c5ca22740af
-	github.com/tinylib/msgp v0.0.0-20180912221530-f65876d3ea05
+	github.com/philhofer/fwd v1.0.0 // indirect
+	github.com/savsgio/gotils v0.0.0-20181217102925-dc772c0d89a4
+	github.com/tinylib/msgp v1.1.0
 )
--- types_gen.go
+++ types_gen.go
@@ -13,12 +13,14 @@ func (z *Dict) DecodeMsg(dc *msgp.Reader) (err error) {
 	var zb0001 uint32
 	zb0001, err = dc.ReadMapHeader()
 	if err != nil {
+		err = msgp.WrapError(err)
 		return
 	}
 	for zb0001 > 0 {
 		zb0001--
 		field, err = dc.ReadMapKeyPtr()
 		if err != nil {
+			err = msgp.WrapError(err)
 			return
 		}
 		switch msgp.UnsafeString(field) {
@@ -26,6 +28,7 @@ func (z *Dict) DecodeMsg(dc *msgp.Reader) (err error) {
 			var zb0002 uint32
 			zb0002, err = dc.ReadArrayHeader()
 			if err != nil {
+				err = msgp.WrapError(err, "D")
 				return
 			}
 			if cap(z.D) >= int(zb0002) {
@@ -37,28 +40,33 @@ func (z *Dict) DecodeMsg(dc *msgp.Reader) (err error) {
 				var zb0003 uint32
 				zb0003, err = dc.ReadMapHeader()
 				if err != nil {
+					err = msgp.WrapError(err, "D", za0001)
 					return
 				}
 				for zb0003 > 0 {
 					zb0003--
 					field, err = dc.ReadMapKeyPtr()
 					if err != nil {
+						err = msgp.WrapError(err, "D", za0001)
 						return
 					}
 					switch msgp.UnsafeString(field) {
 					case "Key":
 						z.D[za0001].Key, err = dc.ReadBytes(z.D[za0001].Key)
 						if err != nil {
+							err = msgp.WrapError(err, "D", za0001, "Key")
 							return
 						}
 					case "Value":
 						z.D[za0001].Value, err = dc.ReadIntf()
 						if err != nil {
+							err = msgp.WrapError(err, "D", za0001, "Value")
 							return
 						}
 					default:
 						err = dc.Skip()
 						if err != nil {
+							err = msgp.WrapError(err, "D", za0001)
 							return
 						}
 					}
@@ -67,6 +75,7 @@ func (z *Dict) DecodeMsg(dc *msgp.Reader) (err error) {
 		default:
 			err = dc.Skip()
 			if err != nil {
+				err = msgp.WrapError(err)
 				return
 			}
 		}
@@ -84,6 +93,7 @@ func (z *Dict) EncodeMsg(en *msgp.Writer) (err error) {
 	}
 	err = en.WriteArrayHeader(uint32(len(z.D)))
 	if err != nil {
+		err = msgp.WrapError(err, "D")
 		return
 	}
 	for za0001 := range z.D {
@@ -95,6 +105,7 @@ func (z *Dict) EncodeMsg(en *msgp.Writer) (err error) {
 		}
 		err = en.WriteBytes(z.D[za0001].Key)
 		if err != nil {
+			err = msgp.WrapError(err, "D", za0001, "Key")
 			return
 		}
 		// write "Value"
@@ -104,6 +115,7 @@ func (z *Dict) EncodeMsg(en *msgp.Writer) (err error) {
 		}
 		err = en.WriteIntf(z.D[za0001].Value)
 		if err != nil {
+			err = msgp.WrapError(err, "D", za0001, "Value")
 			return
 		}
 	}
@@ -126,6 +138,7 @@ func (z *Dict) MarshalMsg(b []byte) (o []byte, err error) {
 		o = append(o, 0xa5, 0x56, 0x61, 0x6c, 0x75, 0x65)
 		o, err = msgp.AppendIntf(o, z.D[za0001].Value)
 		if err != nil {
+			err = msgp.WrapError(err, "D", za0001, "Value")
 			return
 		}
 	}
@@ -139,12 +152,14 @@ func (z *Dict) UnmarshalMsg(bts []byte) (o []byte, err error) {
 	var zb0001 uint32
 	zb0001, bts, err = msgp.ReadMapHeaderBytes(bts)
 	if err != nil {
+		err = msgp.WrapError(err)
 		return
 	}
 	for zb0001 > 0 {
 		zb0001--
 		field, bts, err = msgp.ReadMapKeyZC(bts)
 		if err != nil {
+			err = msgp.WrapError(err)
 			return
 		}
 		switch msgp.UnsafeString(field) {
@@ -152,6 +167,7 @@ func (z *Dict) UnmarshalMsg(bts []byte) (o []byte, err error) {
 			var zb0002 uint32
 			zb0002, bts, err = msgp.ReadArrayHeaderBytes(bts)
 			if err != nil {
+				err = msgp.WrapError(err, "D")
 				return
 			}
 			if cap(z.D) >= int(zb0002) {
@@ -163,28 +179,33 @@ func (z *Dict) UnmarshalMsg(bts []byte) (o []byte, err error) {
 				var zb0003 uint32
 				zb0003, bts, err = msgp.ReadMapHeaderBytes(bts)
 				if err != nil {
+					err = msgp.WrapError(err, "D", za0001)
 					return
 				}
 				for zb0003 > 0 {
 					zb0003--
 					field, bts, err = msgp.ReadMapKeyZC(bts)
 					if err != nil {
+						err = msgp.WrapError(err, "D", za0001)
 						return
 					}
 					switch msgp.UnsafeString(field) {
 					case "Key":
 						z.D[za0001].Key, bts, err = msgp.ReadBytesBytes(bts, z.D[za0001].Key)
 						if err != nil {
+							err = msgp.WrapError(err, "D", za0001, "Key")
 							return
 						}
 					case "Value":
 						z.D[za0001].Value, bts, err = msgp.ReadIntfBytes(bts)
 						if err != nil {
+							err = msgp.WrapError(err, "D", za0001, "Value")
 							return
 						}
 					default:
 						bts, err = msgp.Skip(bts)
 						if err != nil {
+							err = msgp.WrapError(err, "D", za0001)
 							return
 						}
 					}
@@ -193,6 +214,7 @@ func (z *Dict) UnmarshalMsg(bts []byte) (o []byte, err error) {
 		default:
 			bts, err = msgp.Skip(bts)
 			if err != nil {
+				err = msgp.WrapError(err)
 				return
 			}
 		}
@@ -215,6 +237,7 @@ func (z *DictMap) DecodeMsg(dc *msgp.Reader) (err error) {
 	var zb0003 uint32
 	zb0003, err = dc.ReadMapHeader()
 	if err != nil {
+		err = msgp.WrapError(err)
 		return
 	}
 	if (*z) == nil {
@@ -230,10 +253,12 @@ func (z *DictMap) DecodeMsg(dc *msgp.Reader) (err error) {
 		var zb0002 interface{}
 		zb0001, err = dc.ReadString()
 		if err != nil {
+			err = msgp.WrapError(err)
 			return
 		}
 		zb0002, err = dc.ReadIntf()
 		if err != nil {
+			err = msgp.WrapError(err, zb0001)
 			return
 		}
 		(*z)[zb0001] = zb0002
@@ -245,15 +270,18 @@ func (z *DictMap) DecodeMsg(dc *msgp.Reader) (err error) {
 func (z DictMap) EncodeMsg(en *msgp.Writer) (err error) {
 	err = en.WriteMapHeader(uint32(len(z)))
 	if err != nil {
+		err = msgp.WrapError(err)
 		return
 	}
 	for zb0004, zb0005 := range z {
 		err = en.WriteString(zb0004)
 		if err != nil {
+			err = msgp.WrapError(err)
 			return
 		}
 		err = en.WriteIntf(zb0005)
 		if err != nil {
+			err = msgp.WrapError(err, zb0004)
 			return
 		}
 	}
@@ -268,6 +296,7 @@ func (z DictMap) MarshalMsg(b []byte) (o []byte, err error) {
 		o = msgp.AppendString(o, zb0004)
 		o, err = msgp.AppendIntf(o, zb0005)
 		if err != nil {
+			err = msgp.WrapError(err, zb0004)
 			return
 		}
 	}
@@ -279,6 +308,7 @@ func (z *DictMap) UnmarshalMsg(bts []byte) (o []byte, err error) {
 	var zb0003 uint32
 	zb0003, bts, err = msgp.ReadMapHeaderBytes(bts)
 	if err != nil {
+		err = msgp.WrapError(err)
 		return
 	}
 	if (*z) == nil {
@@ -294,10 +324,12 @@ func (z *DictMap) UnmarshalMsg(bts []byte) (o []byte, err error) {
 		zb0003--
 		zb0001, bts, err = msgp.ReadStringBytes(bts)
 		if err != nil {
+			err = msgp.WrapError(err)
 			return
 		}
 		zb0002, bts, err = msgp.ReadIntfBytes(bts)
 		if err != nil {
+			err = msgp.WrapError(err, zb0001)
 			return
 		}
 		(*z)[zb0001] = zb0002
@@ -325,28 +357,33 @@ func (z *KV) DecodeMsg(dc *msgp.Reader) (err error) {
 	var zb0001 uint32
 	zb0001, err = dc.ReadMapHeader()
 	if err != nil {
+		err = msgp.WrapError(err)
 		return
 	}
 	for zb0001 > 0 {
 		zb0001--
 		field, err = dc.ReadMapKeyPtr()
 		if err != nil {
+			err = msgp.WrapError(err)
 			return
 		}
 		switch msgp.UnsafeString(field) {
 		case "Key":
 			z.Key, err = dc.ReadBytes(z.Key)
 			if err != nil {
+				err = msgp.WrapError(err, "Key")
 				return
 			}
 		case "Value":
 			z.Value, err = dc.ReadIntf()
 			if err != nil {
+				err = msgp.WrapError(err, "Value")
 				return
 			}
 		default:
 			err = dc.Skip()
 			if err != nil {
+				err = msgp.WrapError(err)
 				return
 			}
 		}
@@ -364,6 +401,7 @@ func (z *KV) EncodeMsg(en *msgp.Writer) (err error) {
 	}
 	err = en.WriteBytes(z.Key)
 	if err != nil {
+		err = msgp.WrapError(err, "Key")
 		return
 	}
 	// write "Value"
@@ -373,6 +411,7 @@ func (z *KV) EncodeMsg(en *msgp.Writer) (err error) {
 	}
 	err = en.WriteIntf(z.Value)
 	if err != nil {
+		err = msgp.WrapError(err, "Value")
 		return
 	}
 	return
@@ -389,6 +428,7 @@ func (z *KV) MarshalMsg(b []byte) (o []byte, err error) {
 	o = append(o, 0xa5, 0x56, 0x61, 0x6c, 0x75, 0x65)
 	o, err = msgp.AppendIntf(o, z.Value)
 	if err != nil {
+		err = msgp.WrapError(err, "Value")
 		return
 	}
 	return
@@ -401,28 +441,33 @@ func (z *KV) UnmarshalMsg(bts []byte) (o []byte, err error) {
 	var zb0001 uint32
 	zb0001, bts, err = msgp.ReadMapHeaderBytes(bts)
 	if err != nil {
+		err = msgp.WrapError(err)
 		return
 	}
 	for zb0001 > 0 {
 		zb0001--
 		field, bts, err = msgp.ReadMapKeyZC(bts)
 		if err != nil {
+			err = msgp.WrapError(err)
 			return
 		}
 		switch msgp.UnsafeString(field) {
 		case "Key":
 			z.Key, bts, err = msgp.ReadBytesBytes(bts, z.Key)
 			if err != nil {
+				err = msgp.WrapError(err, "Key")
 				return
 			}
 		case "Value":
 			z.Value, bts, err = msgp.ReadIntfBytes(bts)
 			if err != nil {
+				err = msgp.WrapError(err, "Value")
 				return
 			}
 		default:
 			bts, err = msgp.Skip(bts)
 			if err != nil {
+				err = msgp.WrapError(err)
 				return
 			}
 		}
